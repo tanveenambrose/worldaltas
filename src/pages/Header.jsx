89198f3cda63aba94099b8c12a04d3088e549e53
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-14 10:31:37 +0600

    Daisy Ui intstalled

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,23 +4,7 @@ import { NavLink } from 'react-router-dom'
 function Header() {
   return (
     <header>
-      <div className="container">
-        <div className="grid navbar-grid">
-          <div className='Logo'>
-            <NavLink to='/'>
-              <h1>WorldAtlas</h1>
-            </NavLink>
-          </div>
-          <nav>
-            <ul>
-              <li><NavLink to='/'>Home</NavLink></li>
-              <li><NavLink to='/About'>About</NavLink></li>
-              <li><NavLink to='/Contact'>Contact</NavLink></li>
-              <li><NavLink to='/Country'>Country</NavLink></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      
     </header>
   )
 }
