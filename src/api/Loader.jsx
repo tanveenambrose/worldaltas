7268a5cb5b4afe77054b7e7c5d0a489aa016c21a
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-17 16:31:53 +0600

    yeah still doing it

---
+++ src/api/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Spinner } from "flowbite-react";
+
+function Loader() {
+  return (
+    <div>
+      <Spinner aria-label="Default status example" />;
+    </div>
+  )
+}
+
+export default Loader
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useTransition } from 'react'
+import React, { useEffect, useState, useTransition } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import { getCountryData } from '../api/postApi';
+import Loader from '../api/Loader';
 
 
 const Country = () => {
 
   const [isPending, startTransition]= useTransition();
+  const [countries, setCountries]= useState([]);
 
   useEffect(()=>{
     startTransition(async()=>{
       const res = await getCountryData();
-      console.log(res);
-    })
+      setCountries(res.data);
+    });
   },[]);
 
-  if(isPending) return <h1>Loading....</h1>;
+  if(isPending) return <Loader/>;
 
   return (
      <>
     <Header/>
-    <h1>Country Page</h1>
+    <section>
+      <ul>
+        
+      </ul>
+    </section>
     <Footer/>
     </>
   )
