593c58f35b0cc6d2ba936f84f9fe9313bef840f7
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-20 14:17:56 +0600

    ok doing it well

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -32,16 +32,35 @@ function CountryDetails() {
     <>
     <Header/>
     <section className='container mx-auto grid sm:grid-cols-2 gap-2 py-20 bg-gray-600'>
+
       <div className='grid col-span-1 px-4 justify-center'>
-        <NavLink to="/Country"><button className='pb-6 text-md text-white'><IoIosArrowDropleft />Go Back</button></NavLink>
+        <NavLink to="/Country"><button className='border-3 btn bg-amber-600 mb-6 text-md '><IoIosArrowDropleft />Go Back</button></NavLink>
         <img src={countries.flags?.svg} alt={countries.name?.official || "flag"} className='object-cover border-4 w-[300px] h-auto rounded-2xl' />
-        <p className='text-center font-bold text-white font-mono'>{countries.name?.common}</p>
+        <p className='text-center text-xl font-bold text-white font-mono'>{countries.name?.common}</p>
       </div>
+
       <div className='grid col-span-1 text-center my-auto'>
         <p className='text-3xl font-semibold text-yellow-200 mb-6'>{countries.name?.official}</p>
+         <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Native Name : </span> {Object.keys(countries.name.nativeName)
+                    .map((key) => countries.name.nativeName[key].common)
+                    .join(", ")}</p> 
         <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Capital : </span>{countries.capital}</p>
-        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Population : </span>{countries.population}</p>
-        <p></p>
+        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Population : </span>{countries.population.toLocaleString()}</p>
+        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Region : </span>{countries.region}</p>
+        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Sub-Region : </span>{countries.subregion}</p>
+        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Top-Level Domain : </span>{countries.tld[0]}</p>
+         <p className='font-semibold text-yellow-200'>
+                  <span className='text-gray-200'> Currencies: </span>
+                  {Object.keys(countries.currencies)
+                    .map((curElem) => countries.currencies[curElem].name)
+                    .join(", ")}
+                </p>
+        <p className='font-semibold text-yellow-200'>
+                  <span className='text-gray-200'>Languages: </span>
+                  {Object.keys(countries.languages)
+                    .map((key) => countries.languages[key])
+                    .join(", ")}
+                </p>
       </div>
        
         
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
       <Header />
 
       <main className="mx-auto container bg-gray-700 text-white mt-2 rounded-b-2xl px-4 hero-section main">
-        <div className="grid grid-cols-6 gap-4">
+        <div className="grid sm:grid-cols-3 gap-4">
 
-          <div className="col-span-4 hero-content flex flex-col  ">
+          <div className="sm:col-span-2 hero-content flex flex-col">
             <h1 className="text-4xl ">Explore the world, One Country at a Time</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto veritatis, quisquam ex, vel nam minima suscipit corrupti temporibus earum accusamus animi molestias cumque. Quod a natus, rerum repellendus repudiandae aspernatur!</p>
             <button className='btn btn-darken btn-inline bg-white-box space-x-2'>
@@ -23,7 +23,7 @@ const Home = () => {
           </div>
           
     
-            <div className="col-span-2  bg-gray-700 hero-image flex justify-center items-center-safe">
+            <div className="sm:col-span-1  bg-gray-700 hero-image flex justify-center items-center-safe">
               <img src="./world.png" alt="world"  className="hover:rotate-360 transition-transform duration-2000"/>
             </div>
         </div>
