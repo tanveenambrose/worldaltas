402d1068f18958b8429fffc75c561b36451f72b6
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-20 12:30:13 +0600

    ok i am doing it all

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,17 +1,18 @@
 
 import Header from './Header';
 import Footer from './Footer';
-import { useParams} from 'react-router';
+import { NavLink, useParams} from 'react-router-dom';
 import React, { useEffect, useState, useTransition } from 'react';
 import Loader from '../api/Loader';
 import { getCountryIndiviData } from '../api/postApi';
+import { IoIosArrowDropleft } from "react-icons/io";
 
 
 
 function CountryDetails() {
     const params = useParams();
       const [isPending, startTransition]= useTransition();
-      const [country, setCountry]= useState([]);
+      const [countries, setCountry]= useState(null);
 
   useEffect(()=>{
     startTransition(async()=>{
@@ -24,11 +25,27 @@ function CountryDetails() {
   },[]);
 
   if(isPending) return <Loader/>;
+  if(!countries) return null;
+
     console.log(params);
   return (
     <>
     <Header/>
-    <h1>Done</h1>
+    <section className='container mx-auto grid sm:grid-cols-2 gap-2 py-20 bg-gray-600'>
+      <div className='grid col-span-1 px-4 justify-center'>
+        <NavLink to="/Country"><button className='pb-6 text-md text-white'><IoIosArrowDropleft />Go Back</button></NavLink>
+        <img src={countries.flags?.svg} alt={countries.name?.official || "flag"} className='object-cover border-4 w-[300px] h-auto rounded-2xl' />
+        <p className='text-center font-bold text-white font-mono'>{countries.name?.common}</p>
+      </div>
+      <div className='grid col-span-1 text-center my-auto'>
+        <p className='text-3xl font-semibold text-yellow-200 mb-6'>{countries.name?.official}</p>
+        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Capital : </span>{countries.capital}</p>
+        <p className='font-semibold text-yellow-200'><span className='text-gray-200'>Population : </span>{countries.population}</p>
+        <p></p>
+      </div>
+       
+        
+    </section>
     <Footer/>
     </>
   )
