c495c8421e330cc8f13198bad9d423eb9003c69c
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-20 21:20:16 +0600

    ok done

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/worldaltas/',
   plugins: [react(), tailwindcss(), flowbiteReact()],
 })
