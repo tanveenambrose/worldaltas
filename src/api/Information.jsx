b36d75c69b806d12f20c6b6ecda08ea07a1bab0f
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-20 22:05:02 +0600

    ok

--- src/api/Information.jsx
+++ src/api/Information.jsx
@@ -4,10 +4,10 @@ import CountryDatas from "./CountryData.json";
 function Information() {
   return (
     <section className='container mx-auto section-about mb-10'>
-        <h1 className='text-3xl mt-10 text-center font-mono  '>Here Are Some Intresting Facts <br /> we're proud of</h1>
+        <h1 className='text-3xl mt-10  text-center font-mono  '>Here Are Some Intresting Facts <br /> we're proud of</h1>
         
         
-        <div className="card-sections grid lg:grid-cols-3 sm:grid-cols-2  gap-4">
+        <div className="card-sections grid lg:grid-cols-3 sm:grid-cols-2 mx-4  gap-4">
           {
             CountryDatas.map((country)=>{
               const {id,countryName,capital,population,interestingFact} = country;
